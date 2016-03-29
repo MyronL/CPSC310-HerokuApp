4bdbabed672cada8c8ce1124f3fc141f76d6a0c3
Chia-Wen Cheng <b01902026@ntu.edu.tw>
2016-03-29 08:45:35 -0700

    fix editor

--- OverPricedHotdogs/project/routes/modules/country-list.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = [
-    {short:"  " , name:"Please select account type"},
-    {short:"V" , name:"Viewer"},
-    {short:"C" , name:"Contributor"}
-]
--- OverPricedHotdogs/project/routes/modules/email-settings.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-	
-	host		: 'smtp.gmail.com',
-	user 		: 'overpricedhotdogs@gmail.com',
-	password 	: 'overpricedhotdogs1',
-	sender		: 'OverPriced Hotdogs <overpricedhotdogs@gmail.com>'
-	
-}
